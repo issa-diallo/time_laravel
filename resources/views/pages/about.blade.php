4a0e0cedc42e11d6179729bcaa76cf89b2fc26ee
dial1201 <fancy_95310hotmail.com>
2021-09-30 12:52:05 +0200

    :wrench: add proper page title

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,5 +1,7 @@
 @extends('base')
 
+@section('title','about')
+
 @section('content')
   <p>Build with &hearts; by Dévellopeur backend junior lemon learning</p>
 
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,7 +1,9 @@
 @extends('base')
 
-@section('content')
-<h1>Hello World</h1>
+@section('title','home')
 
-<p>It's currently {{date("h:i A")}}</p>
+@section('content')
+  <h1>Hello World</h1>
+
+  <p>It's currently {{date("h:i A")}}</p>
 @endsection
